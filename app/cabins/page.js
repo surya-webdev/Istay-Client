814f00d81b0c8b123e4dff19d0c20a381845727a
surya-webdev <surya.webdev23@gmail.com>
2024-06-25 12:35:18 +0530

    feat: Added suspend load & dynamic cabin

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,72 +1,25 @@
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
-
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-};
+import { Suspense } from "react";
+import CabinData from "@/app/_components/CabinData";
+import Spinner from "@/app/_components/Spinner";
 
 export default function Page() {
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
-
   return (
-    <div className="mx-auto mt-8 max-w-6xl">
-      <div className="mb-24 grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 px-10 py-3">
-        <div className="relative -translate-x-3 scale-[1.15]">
-          <img
-            className="object-cover"
-            fill
-            src={image}
-            alt={`Cabin ${name}`}
-          />
-        </div>
+    <div>
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
+        Our Luxury Cabins
+      </h1>
+      <p className="mb-10 text-lg text-primary-200">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
 
-        <div>
-          <h3 className="mb-5 w-[150%] translate-x-[-254px] bg-primary-950 p-6 pb-1 text-7xl font-black text-accent-100">
-            Cabin {name}
-          </h3>
-
-          <p className="mb-10 text-lg text-primary-300">{description}</p>
-
-          <ul className="mb-7 flex flex-col gap-4">
-            <li className="flex items-center gap-3">
-              <UsersIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                guests
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <MapPinIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Located in the heart of the{" "}
-                <span className="font-bold">Dolomites</span> (Italy)
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Privacy <span className="font-bold">100%</span> guaranteed
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div>
-        <h2 className="text-center text-5xl font-semibold">
-          Reserve today. Pay on arrival.
-        </h2>
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <CabinData />
+      </Suspense>
     </div>
   );
 }
