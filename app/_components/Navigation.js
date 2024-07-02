d089df4317ff00f8792b2947a6e60fdabc9b4671
surya-webdev <surya.webdev23@gmail.com>
2024-07-02 22:32:16 +0530

    fix: fixed auth image

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const { user } = await auth();
+  console.log(user);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex items-center gap-16">
@@ -20,13 +25,29 @@ export default function Navigation() {
             About
           </Link>
         </li>
-        <li>
-          <Link
-            href="/account"
-            className="transition-colors hover:text-accent-400"
-          >
-            Guest area
-          </Link>
+        <li className="flex items-center justify-center gap-4">
+          {user ? (
+            <>
+              <img
+                className="h-10 w-10 rounded-3xl"
+                src={user.image}
+                alt={user.name}
+              />
+              <Link
+                href="/account"
+                className="tran sition-colors hover:text-accent-400"
+              >
+                Guest area
+              </Link>
+            </>
+          ) : (
+            <Link
+              href="/account"
+              className="transition-colors hover:text-accent-400"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
